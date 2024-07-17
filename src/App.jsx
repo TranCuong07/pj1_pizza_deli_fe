ca5a50c777fe88f9206fcae1bc57391381358035
TranCuong <tccuong096@gmail.com>
2024-07-17 14:53:34 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import  "./App.css"
+import FormInput from "./components/FormInput";
+
+const App = () =>{
+    const [username, setUsername] = useState('')
+    
+
+    console.log (username)
+    return <div className="app"> 
+    <form>
+        <FormInput placeholder="Username" setUsername={setUsername} />
+        <FormInput placeholder="Email"/>
+        <FormInput placeholder="Full Name"/>
+        <FormInput/>
+        <FormInput/>
+    </form>
+    </div>;
+};
+export default App;
