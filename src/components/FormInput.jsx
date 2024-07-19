45609d3bbe10ec08bbdeae0d2eadf873553bbab7
TranCuong <tccuong096@gmail.com>
2024-07-19 15:53:17 +0700

    initial commit

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
-import "./formInput.css"; 
+import React, { useState } from 'react'
+import "./formInput.css";
 
 const FormInput = (props) => {
+  const [focused,setFocused] = useState(false);
+  const { label, onChange,id,errorMessage, ...inputProps} = props;
+  const handleFocus = (e) => {
+    setFocused(true);
+  };
   return (
-    <div className="fromInput">
-        <label>Username</label>
-        <input placeholder ={props.placeholder} onChange={e => props.setUsername(e.target.value)}/> 
+    <div className="formInput">
+      <label> {label}</label>
+      <input {...inputProps}
+       onChange={onChange}
+       onBlur={handleFocus}
+       onFocus={()=> 
+        inputProps.name === "confirmPassword" && setFocused(true)
+       }
+       focused ={focused.toString()}
+      /> 
+      <span>{errorMessage}</span>
     </div>
   )
 }
